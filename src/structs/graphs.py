4622485accb8624a3434fc78bdafd4b9ccd4e526
Marek <zelaznymarek@gmail.com>
2020-05-24 16:48:38 +0200

    add dfs graph traversal algorithm

--- src/structs/graphs.py
+++ src/structs/graphs.py
@@ -6,36 +6,52 @@ class Node:
         self.neighbours = []
 
 
-class BFS:
-    """Breadth-first search implementation"""
+def bfs(first_node):
+    """Breadth-first search graph traversing algorithm"""
+    queue = []
+    queue.append(first_node)
 
-    def bfs(self, first_node):
-        queue = []
-        queue.append(first_node)
+    while queue:
+        actual_node = queue.pop(0)
 
-        while queue:
-            actual_node = queue.pop(0)
+        actual_node.visited = True
+        print(actual_node.name)
 
-            actual_node.visited = True
-            print(actual_node.name)
-
-            for node in actual_node.neighbours:
-                if not node.visited and node not in queue:
-                    queue.append(node)
+        for node in actual_node.neighbours:
+            if not node.visited and node not in queue:
+                queue.append(node)
 
 
-node_1 = Node('A')
-node_2 = Node('B')
-node_3 = Node('C')
-node_4 = Node('D')
-node_5 = Node('E')
+def dfs(node):
+    """Depth-first search graph traversing algorithm"""
+    print(node.name)
+    node.visited = True
+
+    for n in node.neighbours:
+        if not n.visited:
+            dfs(n)
 
 
-node_1.neighbours.append(node_2)
-node_1.neighbours.append(node_3)
-node_2.neighbours.append(node_4)
-node_2.neighbours.append(node_3)
-node_4.neighbours.append(node_5)
+def generate_nodes():
+    node_1 = Node('A')
+    node_2 = Node('B')
+    node_3 = Node('C')
+    node_4 = Node('D')
+    node_5 = Node('E')
 
-bfs = BFS()
-bfs.bfs(node_1)
+    node_1.neighbours.append(node_2)
+    node_1.neighbours.append(node_3)
+    node_2.neighbours.append(node_4)
+    node_2.neighbours.append(node_3)
+    node_4.neighbours.append(node_5)
+
+    return node_1
+
+
+node = generate_nodes()
+print('BFS')
+print(bfs(node))
+
+node = generate_nodes()
+print('DFS')
+print(dfs(node))
